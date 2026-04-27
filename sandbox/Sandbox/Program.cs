3cba3950ed297239c0fbb71a7a71a2111fc18b15
Nic King <kin25019@byui.edu>
2026-04-27 14:57:47 -0600

    In class loops

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        // int x = 10;
+
+        // if(x == 11)
+        // {
+        //     Console.Write("Hey Bob");
+
+        // }
+
+        // for (int i = 10000; i >= -10000; i--)
+        // {
+        //     // if (i % 2 == 1)
+        //         Console.WriteLine(i);
+        // }
+
+        int age = -1;
+
+        while (age < 0 || age > 125)
+        {
+            Console.WriteLine("Please write your age: ");
+            age = int.Parse(Console.ReadLine());
+
+        }
     }
 }
