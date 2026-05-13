baf9801a266ee3889b90a4912f523b653eed3f9a
Nic King <kin25019@byui.edu>
2026-05-13 14:53:31 -0600

    added additional journal entry and commented out unneeded write outs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,11 +6,17 @@ class Program
     {
         JournalEntry entry = new JournalEntry();
         entry.CreateJournalEntry();
-        entry.Display();
-        Console.WriteLine(entry.CreateFileSystemString());
+       // entry.Display();
+       // Console.WriteLine(entry.CreateFileSystemString());
+        JournalEntry entry2 = new JournalEntry();
+        entry2.CreateJournalEntry();
+
 
         Journal journal = new Journal();
         journal.AddEntry(entry);
+        journal.AddEntry(entry2);
+
+        
         journal.DisplayEntries();
     }
 }
