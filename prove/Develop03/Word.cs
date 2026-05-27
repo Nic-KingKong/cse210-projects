3f74d7385bf5aee715e7f380f3e369cddbf9fcf5
Nic King <kin25019@byui.edu>
2026-05-27 14:49:21 -0600

    Word class created and word object functional

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,45 @@
+using System;
+
+class Word
+{
+    private string _word;
+    private bool _hidden;
+
+    public Word(string word)
+    {
+        _word = word;
+        _hidden = false;
+    }
+
+    public string GetWordString()
+    {
+        string tempWord = "";
+        if (_hidden)
+        {
+            foreach (char c in _word)
+            {
+                tempWord += '_';
+            }
+        }
+        else
+        {
+            tempWord = _word;
+        }
+        return tempWord;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _hidden;
+    }
+
+    public void displayWord()
+    {
+        Console.WriteLine(GetWordString());
+    }
+}
