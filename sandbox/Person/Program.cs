33e839c4bedb62841c536f3663250b06b67f4d23
Nic King <kin25019@byui.edu>
2026-06-01 16:47:30 -0600

    In class person and police class creation and testing

--- sandbox/Person/Program.cs
+++ sandbox/Person/Program.cs
@@ -3,6 +3,10 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine("Hello World!");
+        Person myPerson = new Person("Bob", "Roberts", 30, 180);
+        Console.WriteLine(myPerson.GetPersonInfo());
+
+        Police myPoliceman = new Police("Gun", "Robert", "Bob", 40, 150);
+        Console.WriteLine(myPoliceman.GetPoliceInfo());
     }
 }
