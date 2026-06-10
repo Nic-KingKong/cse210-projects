76a1f103510002a57a5983effd146287658f80b9
Nic King <kin25019@byui.edu>
2026-06-10 14:53:03 -0600

    keep going

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,35 +2,53 @@ using System;
 
 class Program
 {
+    public void menuDisplay_6()
+{
+    Console.WriteLine("Please select one of the following choices:");
+    Console.WriteLine("1. Write");
+    Console.WriteLine("2. Display");
+    Console.WriteLine("3. Load");
+    Console.WriteLine("4. Save");
+    Console.WriteLine("5. Quit");
+    Console.WriteLine("What would you like to do? ");
+}
     static void Main(string[] args)
     {
-        JournalEntry entry = new JournalEntry();
-        entry.CreateJournalEntry();
-        //entry.Display();
-        //Console.WriteLine(entry.CreateFileSystemString());
-        JournalEntry entry2 = new JournalEntry();
-        entry2.CreateJournalEntry();
+        // JournalEntry entry = new JournalEntry();
+        // entry.CreateJournalEntry();
+        // entry.Display();
+        // Console.WriteLine(entry.CreateFileSystemString());
+        // JournalEntry entry2 = new JournalEntry();
+        // entry2.CreateJournalEntry();
 
 
-        Journal journal = new Journal();
-        journal.AddEntry(entry);
-        journal.AddEntry(entry2);
+        // Journal journal = new Journal();
+        // journal.AddEntry(entry);
+        // journal.AddEntry(entry2);
 
 
-        journal.DisplayEntries();
+        // journal.DisplayEntries();
+        Console.WriteLine("Welcome to the Journal Program!");
+        Program menu = new Program();
+        menu.menuDisplay_6();
+
         int response = 0;
         response = int.Parse(Console.ReadLine());
-
-        switch(response)
+        while(response != 5)
         {
-            case 1:
-                JournalEntry newEntry = new JournalEntry();
-                newEntry.CreateJournalEntry();
-                journal.AddEntry(newEntry);
-                break;
-            case 2:
-                journal.DisplayEntries();
-                break;
+            switch(response)
+            {
+                case 1:
+                    JournalEntry newEntry = new JournalEntry();
+                    newEntry.CreateJournalEntry();
+                    journal.AddEntry(newEntry);
+                    break;
+                case 2:
+                    journal.DisplayEntries();
+                    break;
+            }
+            menu.menuDisplay_6();
+            response = int.Parse(Console.ReadLine());
         }
     }
 }
