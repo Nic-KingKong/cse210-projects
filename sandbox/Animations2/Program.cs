4703d274bd4a3abe46213dd24ef6ccb7ddf5a66c
Nic King <kin25019@byui.edu>
2026-06-10 14:52:24 -0600

    animation changes

---
+++ sandbox/Animations2/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int sleepTime = 1000; // Time in milliseconds
+
+        Console.Clear();
+        Console.CursorVisible = false;
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("+");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b \b");
+
+            Console.Write("-");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b \b");
+        }
+
+        //string animationString = "\\|/-";
+
+        int count = 15;
+        DateTime now = DateTime.Now;
+        DateTime endTime = now.AddSeconds(count); // Run the animation for 10 seconds
+        //for (int i = 0; i < 50; i++)
+        //int index = 0;
+        //while(DateTime.Now < endTime)
+        while (count >= 0)
+        {
+            Console.Write($"{count--,2}");
+            //Console.Write(animationString[index++ % animationString.Length]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b\b");
+
+            // Console.Write("-");
+            // Thread.Sleep(sleepTime);
+            // Console.Write("\b \b");
+        }
+
+        Console.CursorVisible = true;
+
+    }
+}
